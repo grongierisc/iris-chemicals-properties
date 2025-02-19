09f86dca8bc476927cc7135521f7ab3b9e696081
grongierisc <guillaume.rongier@intersystems.com>
2025-02-19 16:52:17 +0100

    refactor: remove redundant tests for IUPAC name and pKa calculations in TestBoRdkit

--- src/python/tests/test_bo_rdkit.py
+++ src/python/tests/test_bo_rdkit.py
@@ -10,9 +10,6 @@ class TestBoRdkit:
     mol = Chem.MolFromSmiles(smi)
     bo = bo_rdkit.RDKitOperation()
 
-    def test_calculate_iupac_name(self):
-        assert self.bo._calculate_iupac_name(self.mol) == "2-[4-(2-methylpropyl)phenyl]propanoic acid"
-
     def test_calculate_mol_formula(self):
         assert self.bo._calculate_mol_formula(self.mol) == "C13H18O2"
 
@@ -30,12 +27,6 @@ class TestBoRdkit:
 
     def test_calculate_tpsa(self):
         assert self.bo._calculate_tpsa(self.mol) == 37.3
-
-    def test_calculate_pka(self,mocker):
-        # mock the self.send_request_sync
-        with mocker.patch.object(self.bo, 'send_request_sync', return_value=msg.PkaResponse(pka=3.0, pka_type='acidic')):
-            pka,pka_type = self.bo._calculate_pka(self.mol)
-            assert pka == 3.0 and pka_type == 'acidic'
 
     def test_calcualte_heavy_atom_count(self):
         assert self.bo._calculate_heavy_atom_count(self.mol) == 15
